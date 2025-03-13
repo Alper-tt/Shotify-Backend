2fbbbe86473ceeec69da17967f2816a836a7461c
Alper-tt <alpertopraktepe46@gmail.com>
2025-03-13 23:20:49 +0300

    firabase storage photo upload

---
+++ backend/src/main/java/com/alper/shotify/backend/config/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package com.alper.shotify.backend.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("/Users/alper/Desktop/shotify-d5ae8-firebase-adminsdk-fbsvc-381075e8be.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("shotify-d5ae8.appspot.com")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
