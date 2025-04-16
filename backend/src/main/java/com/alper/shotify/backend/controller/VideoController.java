8440f5615ef7dde1ae3e446963d3fc0d54c56ca1
Alper-tt <alpertopraktepe46@gmail.com>
2025-04-16 23:50:01 +0300

    16.04

--- backend/src/main/java/com/alper/shotify/backend/controller/VideoController.java
+++ backend/src/main/java/com/alper/shotify/backend/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.alper.shotify.backend.controller;
 import com.alper.shotify.backend.model.request.CreateVideoRequestDTO;
 import com.alper.shotify.backend.model.response.VideoUrlResponseDTO;
 import com.alper.shotify.backend.service.VideoCreationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ public class VideoController {
     final VideoCreationService videoCreationService;
 
     @PostMapping("/create")
+    @Operation(summary = "Yeni video oluştur")
     public ResponseEntity<VideoUrlResponseDTO> createVideo(@RequestBody CreateVideoRequestDTO request) {
         return ResponseEntity.ok(videoCreationService.createVideo(request));
     }
